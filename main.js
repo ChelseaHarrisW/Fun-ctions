c9e5e7dfda6097ef8f2afc868b6a3928f578786e
Chelsea.harris <chelsea.harris@asurion.com>
2022-01-06 13:36:23 -0600

    calculations

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ const fillGasTank = (gallons) => {
 console.log(fillGasTank(15))
 console.log(fillGasTank(22))
 
-// how functions call your name and
+// how functions call your name 
 
 /*
     Define a function with a parameter. This means the
@@ -67,35 +67,42 @@ console.log(jenna)
 
 const rotateCake = () => {
 	// Code to rotate the cake
-    console.log
+   return console.log ( 'rotate the cake')
 }
 
 const gatherIngredients = () => {
 	// Code to gather the ingredients from storage
+    return console.log( 'get ingredients from pantry to prepare for measuring')
 }
 
 const measureIngredients = () => {
 	// Code to measure the ingredients
+    return console.log( 'measure ingredients')
 }
 
 const addFrosting = () => {
 	// Code to add frosting to a baked cake
+    console.log( 'add you favorite frosting to a baked, and cooled cake')
 }
 
 const startOven = () => {
 	// Code to turn on the oven to a specific temperature
+    console.log('preheat your oven to 345')
 }
 
 const createBatter = () => {
 	// Code to create some cake batter
+    console.log('create batter by mixing together all measured ingredients')
 }
 
 const checkIfDone = () => {
 	// Code to check if the cake is done
+    console.log('at 30 minitues take a toothpick and poke the center of the cake to check doneness')
 }
 
 const startBaking = () => {
 	// Code to put the cake batter into the oven
+    console.log('place cake in the oven')
 }
 
 startOven()
@@ -106,3 +113,145 @@ startBaking()
 rotateCake()
 checkIfDone()
 addFrosting()
+
+//Deconstruction Mode
+// how functions write leters
+const getPaper = () => {
+    return console.log('Get a piece of paper')
+}
+const foldPaper = () => {
+    return console.log('Fold it in half')
+}
+
+const penPrep = () => {
+    return console.log('Clean my fountain pen')
+}
+
+const penFill = () => {
+    console.log('Fill fountain pen with ink color I want')
+}
+
+const write = () => {
+    console.log('Write letter')
+}
+const foldPaper2 = () => {
+    console.log('Fold letter to make it into an envelope to be addressed')
+}
+
+const meltWax = () => {
+    console.log('Melt wax colors I want for the seal')
+}
+
+const pourWax = () => {
+    console.log('Pour wax onto letter')
+}
+const sealPress = () => {
+    console.log('Press seal into wax')
+}
+
+const prepEnvelope = () => {
+    console.log('Address the envelope')
+}
+
+const sendMail = () => {
+    console.log('Mail letter')
+}
+
+const stamp = () => {
+    console.log('Add stamp to envelope')
+}
+
+getPaper()
+penPrep()
+penFill()
+write()
+foldPaper()
+foldPaper2()
+pourWax()
+meltWax()
+sealPress()
+prepEnvelope()
+stamp()
+sendMail()
+
+//calculator deconstruction of larger tasks
+
+//additional tasks
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+let resulta = add(17, 9)
+console.log(resulta)
+
+resulta = add(-13, -7)
+console.log(resulta)
+
+resulta = add(4, 58)
+console.log(resulta)
+
+// subtractional
+const subtract = (firstNumber, secondNumber) => {
+    const minus = firstNumber - secondNumber
+    return minus
+}
+
+let results = subtract(17, 9)
+console.log(results)
+
+results = subtract(-13, -7)
+console.log(results)
+
+results = subtract(4, 58)
+console.log(results)
+
+// multipliciity 
+const multiply = (firstNumber, secondNumber) => {
+    const times = firstNumber * secondNumber
+    return times
+}
+
+let resultm = multiply(17, 9)
+console.log(resultm)
+
+resultm = multiply(-13, -7)
+console.log(resultm)
+
+resultm = multiply(4, 58)
+console.log(resultm)
+
+// divisional
+const divide = (firstNumber, secondNumber) => {
+    const dividedBy = firstNumber / secondNumber
+    return dividedBy
+}
+
+let resultd = divide(17, 9)
+console.log(resultd)
+
+resultd = divide(-13, -7)
+console.log(resultd)
+
+resultd = divide(4, 58)
+console.log(resultd)
+
+// squared
+const square = (firstNumber) => {
+    const squared = firstNumber * firstNumber
+    return squared
+}
+
+let result2 = square(17)
+console.log(result2)
+
+result2 = square(-13)
+console.log(result2)
+
+result2 = square(4)
+console.log(result2)
+
+
+//
+
+
